6dcbce3a2faf2fbd3b95e57b2a62990a67f62ccb
Maciek K <krzyzanowski.m95@gmail.com>
2022-07-18 21:15:13 +0200

    Deprecated WebSecurityConfigAdapter

--- src/main/java/com/example/redditclone/config/SecurityConfig.java
+++ src/main/java/com/example/redditclone/config/SecurityConfig.java
@@ -1,7 +1,8 @@
 package com.example.redditclone.config;
 
-import com.example.redditclone.service.UserService;
+import com.example.redditclone.model.User;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,38 +11,40 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private final UserService userService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll()
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userService);
-        auth.authenticationProvider(provider);
-    }
+public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                auth
+                        .antMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated());
+
+        http.formLogin(form ->
+                form.loginPage("/login").permitAll());
+
+        http.logout(logout ->
+                logout.logoutUrl("/logout").logoutSuccessUrl("/login"));
+
+        http.csrf(csrf ->
+                csrf.disable());
+
+        return http.build();
+    }
 }
